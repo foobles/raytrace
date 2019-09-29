812035420171eb96422d100ea195d00e7bae6428
foobles <buzdav2@gmail.com>
2019-09-28 20:51:17 -0500

    moved random_in_unit_sphere to crate-level

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,18 @@ use mat::Lambertian;
 
 use rand::prelude::*;
 
+pub fn random_in_unit_sphere() -> Vec3 {
+    //let mut rng = thread_rng();
+    loop {
+        let p = Vec3::new(random(), random(), random()) * 2.0 - Vec3::new(1.0, 1.0, 1.0);
+        if p.length_squared() < 1.0 {
+            return p;
+        } else {
+            continue;
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 struct Camera {
     origin: Vec3,
--- src/mat/lambertian.rs
+++ src/mat/lambertian.rs
@@ -1,6 +1,7 @@
 use super::Material;
 use crate::vector::{Vec3, Ray};
 use crate::hit::HitRecord;
+use crate::random_in_unit_sphere;
 
 use rand::prelude::*;
 
@@ -18,17 +19,5 @@ impl Material for Lambertian {
     fn scatter(&self, ray: Ray, rec: HitRecord) -> Option<(Vec3, Ray)> {
         let target_dir = rec.normal + random_in_unit_sphere();
         Some((self.albedo, Ray::new(rec.point, target_dir)))
-    }
-}
-
-fn random_in_unit_sphere() -> Vec3 {
-    //let mut rng = thread_rng();
-    loop {
-        let p = Vec3::new(random(), random(), random()) * 2.0 - Vec3::new(1.0, 1.0, 1.0);
-        if p.length_squared() < 1.0 {
-            return p;
-        } else {
-            continue;
-        }
     }
 }
