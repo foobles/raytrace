b5fcc6c7c024be9cf8dd5cee993e4c0ca41c65b3
foobles <buzdav2@gmail.com>
2019-10-03 09:43:31 -0500

    Added refract function to vector module

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -8,3 +8,13 @@ pub fn reflect(v: Vec3, n: Vec3) -> Vec3 {
     v - n * v.dot(n)*2.0 // ???
 }
 
+pub fn refract(v: Vec3, n: Vec3, ni_over_nt: f64) -> Option<Vec3> {
+    let uv = v.normalize();
+    let dt = uv.dot(n);
+    let disc = 1.0 - ni_over_nt * ni_over_nt * (1.0-dt*dt);
+    if disc > 0.0 {
+        Some((uv - n*dt)*ni_over_nt - n * disc.sqrt())
+    } else {
+        None
+    }
+}
